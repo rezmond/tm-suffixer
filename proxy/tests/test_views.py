84f285fb8c6f733cd5455043fd9d8a99178fda4a
rezmond <maaaloli@gmail.com>
2019-09-25 14:51:02 +0300

    implements the test for the case when doctype is breaks down

--- proxy/tests/test_views.py
+++ proxy/tests/test_views.py
@@ -21,3 +21,12 @@ class ViewsTestCases(TestCase, SnapshotTestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertMatchSnapshot(response.content.decode('utf-8'))
+
+    def test_doctype(self):
+        client = Client()
+
+        with patch_response(b'<!DOCTYPE html><html><body>test</body></html>'):
+            response = client.get('/test')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertMatchSnapshot(response.content.decode('utf-8'))
